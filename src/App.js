2246d81e6047427192acd9f63471f241af0c7718
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-01-19 02:23:53 +0530

    virtual DOM diffing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "./components/UI/Button/Button";
 
 import './App.css'
 
 const App = () => {
+  const [showParagraph, setShowParagraph] = useState(false)
+
+  const toggleShowParagraphHandler = () => {
+    setShowParagraph((preState) => !preState) //short form of inversing boolean value
+  }
+
   return(
     <div className="app">
       <h1>Hi, there.!</h1>
-      <Button>Show Paragraph</Button>
+      {showParagraph && <p>This is new</p>}
+      <Button onClick={toggleShowParagraphHandler}>Show Paragraph</Button>
     </div>
   )
 }
 
 export default App
+
+// virtual DOM diffing
+// finding out the difference between two snapshots (current & previous state) and update difference in the real DOM
